a044b1b5f05c9b5e6d53aa5cc22dd7478c6d8114
rojomaldit <lucaserfurt@hotmail.com>
2019-08-18 14:46:52 -0300

    change cstoken

--- t2.py
+++ t2.py
@@ -1,12 +1,23 @@
 # importing the requests library 
 import requests 
 import json
-  
-# api-endpoint 
+from bs4 import BeautifulSoup
+import urllib2
+import requests
+
+#Get dates
 URL = "http://comedor.unc.edu.ar/reserva"
 
+page = urllib2.urlopen(URL)
+soup = BeautifulSoup(page, 'html.parser')
+
+cstoken = soup.find('input', attrs={'id': 'cstoken'})
+cstoken = cstoken["value"]
+print(cstoken)
+  
+# api-endpoint 
+
 boundary = "16216400248655893201210934546"
-token = "BEeuMPwSHLe5dCrGhi7W6fiK0VWW1sQjb2nmrFwqw5o="
 user = "0475D9542DE289D"
 
 login_data = "--" + boundary + "\nContent-Disposition: form-data; name=\"cstoken\"\n\n" + token  + "\n--" + boundary + "\nContent-Disposition: form-data; name=\"form_2689_datos\"\n\n" + "ingresar" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"form_2689_datos_implicito\"\n\n" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"ef_form_2689_datosusuario\"\n\n" + user + "\n--" + boundary  + "--"
@@ -18,5 +29,5 @@ data = {
 }
 
 
-r = requests.post(url = URL, data = data, headers=hee)
-print(r.text)
+r = requests.post(url = URL, data = data)
+#print(r.text)
