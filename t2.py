1a8edb0df41b58175c847b6f0a9e897fccc3ce70
rojomaldit <lucaserfurt@hotmail.com>
2019-08-18 14:59:15 -0300

    add ai and ah

--- t2.py
+++ t2.py
@@ -11,16 +11,17 @@ URL = "http://comedor.unc.edu.ar/reserva"
 page = urllib2.urlopen(URL)
 soup = BeautifulSoup(page, 'html.parser')
 
-cstoken = soup.find('input', attrs={'id': 'cstoken'})
-cstoken = cstoken["value"]
-print(cstoken)
+cstoken = soup.find('input', attrs={'id': 'cstoken'})["value"]
+ah = str(soup.find('form', attrs={'id': 'formulario_toba'})['action']).split('&')[0].split('=')[1]
+ai = str(soup.find('form', attrs={'id': 'formulario_toba'})['action']).split('&')[1].split('=')[1]
+print(cstoken, ah, ai)
   
 # api-endpoint 
 
 boundary = "16216400248655893201210934546"
 user = "0475D9542DE289D"
 
-login_data = "--" + boundary + "\nContent-Disposition: form-data; name=\"cstoken\"\n\n" + token  + "\n--" + boundary + "\nContent-Disposition: form-data; name=\"form_2689_datos\"\n\n" + "ingresar" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"form_2689_datos_implicito\"\n\n" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"ef_form_2689_datosusuario\"\n\n" + user + "\n--" + boundary  + "--"
+login_data = "--" + boundary + "\nContent-Disposition: form-data; name=\"cstoken\"\n\n" + cstoken  + "\n--" + boundary + "\nContent-Disposition: form-data; name=\"form_2689_datos\"\n\n" + "ingresar" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"form_2689_datos_implicito\"\n\n" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"ef_form_2689_datosusuario\"\n\n" + user + "\n--" + boundary  + "--"
 
 
 data = {
