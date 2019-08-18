e4a8a44e75c2395843d88c6ab833b3ecee8dcffc
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-08-18 15:49:11 -0300

    Minor changes

--- t2.py
+++ t2.py
@@ -6,24 +6,25 @@ import urllib2
 import requests
 
 #Get dates
-URL = "http://comedor.unc.edu.ar/reserva/"
+URL = 'http://comedor.unc.edu.ar/reserva/'
 
 page = urllib2.urlopen(URL)
 soup = BeautifulSoup(page, 'html.parser')
 
-cstoken = soup.find('input', attrs={'id': 'cstoken'})["value"]
+cstoken = soup.find('input', attrs={'id': 'cstoken'})['value']
 form = str(soup.find('form', attrs={'id': 'formulario_toba'})['action'])
 ah = form.split('&')[0].split('=')[1]
 ai = form.split('&')[1].split('=')[1]
-path = URL + "aplicacion.php?" + "ah=" + ah + "&ai=" + ai
+path = URL + 'aplicacion.php?' + 'ah=' + ah + '&ai=' + ai
 print(cstoken, ah, ai, path)
   
 # api-endpoint 
 
-boundary = "16216400248655893201210934546"
-user = "0475D9542DE289D"
+boundary = '16216400248655893201210934546'
+user1 = '0475D9542DE289D'
+user2 = '04750519BBC6FAC'
 
-login_data = "--" + boundary + "\nContent-Disposition: form-data; name=\"cstoken\"\n\n" + cstoken  + "\n--" + boundary + "\nContent-Disposition: form-data; name=\"form_2689_datos\"\n\n" + "ingresar" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"form_2689_datos_implicito\"\n\n" + "\n--" + boundary  + "\nContent-Disposition: form-data; name=\"ef_form_2689_datosusuario\"\n\n" + user + "\n--" + boundary  + "--"
+login_data = '--' + boundary + '\nContent-Disposition: form-data; name=\'cstoken\'\n\n' + cstoken  + '\n--' + boundary + '\nContent-Disposition: form-data; name=\'form_2689_datos\'\n\n' + 'ingresar' + '\n--' + boundary  + '\nContent-Disposition: form-data; name=\'form_2689_datos_implicito\'\n\n' + '\n--' + boundary  + '\nContent-Disposition: form-data; name=\'ef_form_2689_datosusuario\'\n\n' + user1 + '\n--' + boundary  + '--'
 
 
 query_str_params = {
@@ -35,7 +36,7 @@ form_data = {
     'cstoken' : cstoken,
     'form_2689_datos' : 'ingresar',
     'form_2689_datos_implicito' : '',
-    'ef_form_2689_datosusuario' : user
+    'ef_form_2689_datosusuario' : user1
 }
 
 
